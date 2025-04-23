b8439c81b595c46e765c05ab729d167405f43b61
zekooo0 <ahmedzakria102@gmail.com>
2025-04-23 18:25:10 +0200

    feat: Update layout and home page to include ThemeProvider and ServiceAreaSection for improved structure and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/header/Header";
 import ContactUsSection from "@/sections/contact-us/contact-us-section";
+import ServiceAreaSection from "@/sections/service-area/service-area-section";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <ContactUsSection />
+      <ServiceAreaSection />
     </div>
   );
 }
