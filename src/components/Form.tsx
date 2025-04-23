91a32503c7d3377f00408d4e961d41cb36370475
zekooo0 <ahmedzakria102@gmail.com>
2025-04-23 04:58:16 +0200

    feat: Add Form component and associated styles for user input fields

---
+++ src/components/Form.tsx
@@ -0,0 +1,15 @@
+export default function Form() {
+  return (
+    <form>
+      <div>
+        <div>
+          <input placeholder="Name" />
+          <input placeholder="Email" />
+        </div>
+        <input placeholder="Phone" />
+        <input placeholder="Adress" />
+        <textarea placeholder="Message" />
+      </div>
+    </form>
+  );
+}
